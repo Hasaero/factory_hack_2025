9e60ec93f9b5a3f5ebaf08898e2643d026a23675
JuyoungHa <7infoheonou@gmail.com>
2025-01-07 19:35:21 +0900

    model path

--- Hack_modeling.py
+++ Hack_modeling.py
@@ -179,21 +179,7 @@ def inference(demo_df):
         y = grouped[target].first().values
         serials = grouped['SerialNo'].first().values  # SerialNo 리스트
         # 3. 모델 로드 및 예측
-        # 모델 파일 경로 생성 함수
-        def get_model_path(target):
-            # 현재 파일 위치를 기준으로 절대 경로 생성
-            base_dir = Path(__file__).resolve().parent  # 현재 실행 중인 파일의 디렉토리
-            model_path = base_dir / "main" / "models" / f"{target}_inception.pkl"
-            return model_path
-
-        model_path = get_model_path(target)
-
-        # 모델 로드
-        if model_path.exists():
-            mv_clf = load_learner(model_path)
-        else:
-            raise FileNotFoundError(f"모델 파일을 찾을 수 없습니다: {model_path}")
-        
+        mv_clf = load_learner(f"https://raw.githubusercontent.com/Hasaero/factory_hack_2025/master/models/{target}_inception.pkl")  # 모델 로드
         X_test = X[splits[1]]  # 테스트 데이터
         y_test = y[splits[1]]  # 테스트 타겟값
         probas, _, preds = mv_clf.get_X_preds(X_test, y_test)  # 확률, 실제값, 예측값
